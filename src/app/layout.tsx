acc9d2a1c701ba2e8912c59ebdeb6cc301056cbc
NXSTYone <franck93@mail.ru>
2026-05-25 13:10:49 +0300

    update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,11 @@ import { CartProvider } from "@/context/CartContext";
 export const metadata: Metadata = {
   title: "ШПИНАТ",
   description: "Эко-продукты и полезное питание",
+
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({
@@ -22,11 +27,17 @@ export default function RootLayout({
 
             <footer className="border-t border-[#b89c6d]/40 bg-[#d7c29a] px-5 pb-32 pt-6 text-center md:pb-6">
               <div className="mx-auto flex max-w-7xl flex-col items-center justify-center gap-3 text-sm font-bold text-[#355f28] md:flex-row md:gap-8">
-                <a href="/privacy" className="transition hover:text-[#39A935]">
+                <a
+                  href="/privacy"
+                  className="transition hover:text-[#39A935]"
+                >
                   Политика конфиденциальности
                 </a>
 
-                <a href="/terms" className="transition hover:text-[#39A935]">
+                <a
+                  href="/terms"
+                  className="transition hover:text-[#39A935]"
+                >
                   Пользовательское соглашение
                 </a>
               </div>
